890fd356996546b70da6ff77bae22c0e0ac6174d
Sergiy <barsulsergij@gmail.com>
2023-05-29 20:33:06 -0700

    Saving toggle settings in localstorage

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,31 @@
+const setColorMode = () => {
+  localStorage.getItem('colorMode') == 'dark'
+    ? setDarkMode()
+    : setLightkMode();
+};
+
+const setDarkMode = () => {
+  document.querySelector('body').classList = 'dark';
+  document.querySelector('.toggle__button').style.left = '3px';
+};
+
+const setLightkMode = () => {
+  document.querySelector('body').classList = 'light';
+  document.querySelector('.toggle__button').style.left = '27px';
+};
+
+setColorMode();
+
 const radioButtons = document.querySelectorAll('.toggle__wrapper input');
+
 for(let i = 0; i < radioButtons.length; i++){
   radioButtons[i].addEventListener('click', event => {
-    document.getElementById('dark').checked
-      ? document.querySelector('body').classList = 'dark'
-      : document.querySelector('body').classList = 'light';
+    if (document.getElementById('dark').checked){
+      localStorage.setItem('colorMode', 'dark');
+      setDarkMode();
+    }else{
+      localStorage.setItem('colorMode', 'light');
+      setLightkMode();
+    }
   });
-}
+};
